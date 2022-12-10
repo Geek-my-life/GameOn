bea2ae9cdbcec84db60d090cb081cf6c3202310d
David <david.houesnard@gmail.com>
2022-12-10 16:41:44 +0100

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,22 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnClose = document.querySelector (".close");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById ("form");
+const validForm = document.querySelector(".validationForm");
+
+
+// Const pour récupérer les éléments du formulaire
+const firstName = document.getElementById ("first");
+const lestName = document.getElementById ("last");
+const mail = document.getElementById ("email");
+const birthDate = document.getElementById ("birthdate");
+const quantity = document.getElementById ("quantity");
+const locationTournament = document.getElementsByName ("location");
+const condition = document.getElementById ("checkbox1");
+const newsletter = document.getElementById ("checkbox2");
+const btnSubmit = document.querySelector (".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +34,4 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 
