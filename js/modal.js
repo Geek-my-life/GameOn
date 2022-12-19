e1c4417beb64f67e7779f4e5401d6cda8da97e5c
David <david.houesnard@gmail.com>
2022-12-19 01:18:41 +0100

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// EDIT NAV
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,58 +8,24 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// dom elements formulaire
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const modalBtnClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-const form = document.getElementById("form");
-const validForm = document.getElementById("validationForm");
+const closeBtn = document.getElementsByClassName("close");
 
-// Const pour récupérer les éléments du formulaire
-const firstName = document.getElementById("first");
-const lestName = document.getElementById("last");
-const mail = document.getElementById("email");
-const birthDate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
-const locationTournament = document.getElementsByName("location");
-const condition = document.getElementById("checkbox1");
-const newsletter = document.getElementById("checkbox2");
-const btnSubmit = document.getElementById("btn-submit");
-const boxSubmit = document.getElementById("validatebox");
-const submitBg = document.getElementById("bgroundValidateBox");
-const btnValidate = document.getElementById("btnValidate");
+// lancement formulaire event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// lancement modal formulaire
+// lancement formulaire
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// lancement modal formulaire event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// fermeture formulaire event
+closeBtn[0].addEventListener("click", closeModal);
 
-// fermeture modal formulaire
+// fermeture formulaire
 function closeModal() {
   modalbg.style.display = "none";
 }
-
-// fermeture modal formulaire event
-modalBtnClose.addEventListener("click", closeModal);
-
-// validation formulaire
-function validate(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  submitBg.style.display = "block";
-}
-// validation formulaire event
-validForm.addEventListener("submit", validate);
-
-// fermeture modal formulaire
-function closeValidate() {
-  submitBg.style.display = "none";
-  modalbg.style.display = "none";
-}
-
-// fermeture formulaire OK event
-btnValidate.addEventListener("click", closeValidate);
