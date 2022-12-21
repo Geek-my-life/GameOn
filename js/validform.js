e00f955640c1cf5b756261becc12a732e25ed94b
David <david.houesnard@gmail.com>
2022-12-21 23:35:31 +0100

    Update validform.js

--- js/validform.js
+++ js/validform.js
@@ -9,17 +9,70 @@ const locations = document.querySelectorAll("#allLocations .checkbox-input");
 const checkbox1 = document.getElementById("checkbox1");
 const input = document.getElementsByClassName("text-control");
 const form = document.getElementById("form");
-const regex = /^[a-zA-Z--_]+$/;
+const regEx = /^([A-Za-z]){2,}?([-]){0,}?([A-Za-z]){0,}/;
+const regExMail =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// verification prénom
-function checkFirstName() {
-  if (firstName.value.length < 2 || !firstName.value.match(regex)) {
-    firstName.parentElement.setAttribute("data-error-visible", "true");
-    firstName.style.border = "2px solid #e54858";
+// verification prénom nom
+firstName.addEventListener(
+  "input", // event à la saisie
+  function () {
+    // on appelle une fonction
+    checkInputText(firstName); // la fonction et l'element que l'on souhaite
+  },
+  false
+);
+lastName.addEventListener(
+  "input",
+  function () {
+    checkInputText(lastName);
+  },
+  false
+);
+
+function checkInputText(element) {
+  if (!element.value.match(regEx)) {
+    element.parentElement.setAttribute("data-error-visible", "true");
+    element.style.border = "2px solid #e54858";
     return false;
   } else {
-    first.parentElement.setAttribute("data-error-visible", "false");
-    first.style.border = "solid #279e7a 0.19rem";
+    element.parentElement.setAttribute("data-error-visible", "false");
+    element.style.border = "solid #279e7a 0.19rem";
     return true;
   }
 }
+
+// verification mail
+email.addEventListener("input", checkEmail);
+
+function checkEmail() {
+  if (!email.value.match(regExMail)) {
+    email.parentElement.setAttribute("data-error-visible", "true");
+    email.style.border = "2px solid #e54858";
+    return false;
+  } else {
+    email.parentElement.setAttribute("data-error-visible", "false");
+    email.style.border = "solid #279e7a 0.19rem";
+    return true;
+  }
+}
+
+// verification anniversaire
+
+// verification participation tournoi
+quantity.addEventListener("input", checkQuantity);
+
+function checkQuantity() {
+  if (quantity.value.length === 0 || quantity.value < 0) {
+    quantity.parentElement.setAttribute("data-error-visible", "true");
+    quantity.style.border = "2px solid #e54858";
+    return false;
+  } else {
+    quantity.parentElement.setAttribute("data-error-visible", "false");
+    quantity.style.border = "solid #279e7a 0.19rem";
+    return true;
+  }
+}
+// verification villes
+
+// verification conditions
